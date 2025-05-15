99d1b9b427edd4ca101d28030c592446e9e69dec
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-14 22:40:23 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lista2erx2
+namespace lista2erx3
 {
     internal class Program
     {
+        private const string V = " o maior numero é:";
+
         static void Main(string[] args)
         {
             Console.WriteLine("digite o primeiro numero");
-            int numero1 =
-                int.Parse(Console.ReadLine());
+            int numero1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("digite o segundo numero");
-            int numero2 =
-                int.Parse(Console.ReadLine());
+            int numero2 = int.Parse(Console.ReadLine());
 
-            if (numero1 != numero2)
-            {
-                Console.WriteLine("o maior numero é:" + (numero1 > numero2 ? numero1 : numero2));
-            }
-            else
-            {
-                Console.WriteLine("os numeros são iguais, escreva numeros diferetes.");
-                    }
+            Console.WriteLine("digite o terceiro numero");
+            int numero3 = int.Parse(Console.ReadLine());
+
+            int maior = Math.Max(numero1,
+            Math.Max(numero2, numero3));
+            Console.WriteLine("o numero maior é´:" + maior);
 
         }
     }
