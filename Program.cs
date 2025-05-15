75285a5dd6da8406d79bba30b71332f69bf6b4cc
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-14 22:06:35 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lista2exr1
+namespace lista2erx2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("digite o primeiro numero:");
-            var numero1 =
-            int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o primeiro numero");
+            int numero1 =
+                int.Parse(Console.ReadLine());
 
             Console.WriteLine("digite o segundo numero");
             int numero2 =
-            int.Parse(Console.ReadLine());
+                int.Parse(Console.ReadLine());
 
             if (numero1 != numero2)
             {
-                Console.WriteLine("o maior numero é: " + (numero1 > numero2 ? numero1 : numero2));
+                Console.WriteLine("o maior numero é:" + (numero1 > numero2 ? numero1 : numero2));
             }
+            else
+            {
+                Console.WriteLine("os numeros são iguais, escreva numeros diferetes.");
+                    }
+
         }
     }
 }
