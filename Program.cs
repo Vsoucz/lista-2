71fe81774175a1d43ade1c209c44123943b631a6
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-14 21:49:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2exr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro numero:");
+            var numero1 =
+            int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo numero");
+            int numero2 =
+            int.Parse(Console.ReadLine());
+
+            if (numero1 != numero2)
+            {
+                Console.WriteLine("o maior numero é: " + (numero1 > numero2 ? numero1 : numero2));
+            }
+        }
+    }
+}
